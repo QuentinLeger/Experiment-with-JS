edc01e2a1bc7ff96426a801d42827202964d4983
Quentin LEGER <quentin.leger1@etu.univ-lorraine.fr>
2026-03-29 22:14:48 +0200

    Ajout des fichiers de base

---
+++ script.js
@@ -0,0 +1,48 @@
+const bande = document.querySelector(".bande");
+const cartes = document.querySelectorAll(".carte");
+const nextBtn = document.querySelector(".next");
+const prevBtn = document.querySelector(".prev");
+
+let position = 0;
+let index = 2;
+const largeur = cartes[0].offsetWidth;
+
+updateClasses();
+nextBtn.addEventListener("click", () => {
+
+    index++;
+    position -= largeur;
+    bande.style.transform = `translateX(${position}px)`;
+    updateClasses();
+
+})
+
+prevBtn.addEventListener("click", () => {
+    index--;
+    position += largeur;
+    bande.style.transform = `translateX(${position}px)`;
+    updateClasses();
+
+})
+
+
+
+
+function updateClasses(){
+    cartes.forEach(carte => {
+        carte.classList.remove("active", "side","ultraSide","disparu");
+    });
+
+    cartes[index].classList.add("active");
+
+    if (cartes[index - 1]) cartes[index - 1].classList.add("side");
+    if (cartes[index + 1]) cartes[index + 1].classList.add("side");
+
+    if (cartes[index - 2]) cartes[index - 2].classList.add("ultraSide");
+    if (cartes[index + 2]) cartes[index + 2].classList.add("ultraSide");
+
+    if (cartes[index - 3]) cartes[index - 3].classList.add("disparu");
+    if (cartes[index + 3]) cartes[index + 3].classList.add("disparu");
+}
+
+
